df5964ff3c98fd06537bc8462743c86af80f51c2
LoPhawL <r.harip3@gmail.com>
2023-01-23 01:06:28 +0000

    feat: add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'budgets';
+  categoriesList: any = [];
+
+  async ngOnInit() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyD7amcWHOXMwWBlhWuCYua1f61zzZAGKfA",
+      authDomain: "budgets2.firebaseapp.com",
+      projectId: "budgets2",
+      storageBucket: "budgets2.appspot.com",
+      messagingSenderId: "831553231670",
+      appId: "1:831553231670:web:24ddf858705232e24a3197",
+      measurementId: "G-S9VJX2R51C"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const db = getFirestore(app);
+
+    const categoriesCol = collection(db, 'categories');
+    const categorySnapshot = await getDocs(categoriesCol);
+    this.categoriesList = categorySnapshot?.docs?.map(doc => doc.data()) || [];
+  }
 }
