e1d65b0d676aaa84340180396570938e6bbb289a
HariPrasath <r.harip3@gmail.com>
2023-01-25 01:03:57 +0000

    feat: add progress bar height setting chore: add fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { collection, getDocs, getFirestore, setDoc } from "firebase/firestore";
 import { FirestoreService } from './_common/_services/Firestore.service';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { doc, onSnapshot } from "firebase/firestore";
+
 
 @Component({
   selector: 'app-root',
@@ -10,18 +13,53 @@ import { FirestoreService } from './_common/_services/Firestore.service';
 export class AppComponent {
   title = 'budgets';
 
+  icons = {
+    faXmark
+  }
+
+  public masterExpenditureProgressHeight = 0;
+  public masterExpenditureProgressHeightNoEffectAfterUpdate = false;
+
   constructor(private _fsService: FirestoreService) {}
 
   async ngOnInit() {
 
-    const categoriesCol = collection(this._fsService.db, 'categories');
-    
-    const categorySnapshot = await getDocs(categoriesCol);
-    console.log(categorySnapshot?.docs?.map(doc => doc.data()));
+    // read and set masterProgressHeight
 
-    const settingsCol = collection(this._fsService.db, 'settings');
-    const settingsSnapshot = (await getDocs(settingsCol)).docChanges();
-    console.log(settingsSnapshot);
+    onSnapshot(doc(this._fsService.db, 'settings', 'display', 'masterExpenditureProgressBar', 'height'), (doc) => {
+      const data = doc.data() || {};
+      if (this.masterExpenditureProgressHeightNoEffectAfterUpdate) {
+        this.masterExpenditureProgressHeightNoEffectAfterUpdate = false;
+      } else {
+        this.masterExpenditureProgressHeight = data['value'];
+      }
+    });
+
+    // onSnapshot(doc(this._fsService.db, 'settings', 'display', 'mepb'), (doc) => {
+    //   console.log(doc.data())
+    // });
+
+    // const categoriesCol = collection(this._fsService.db, 'categories');
+
+    // const categorySnapshot = await getDocs(categoriesCol);
+    // console.log(categorySnapshot?.docs?.map(doc => doc.data()));
+
+    // const settingsCol = collection(this._fsService.db, 'settings');
+    // const settingsSnapshot = (await getDocs(settingsCol)).docChanges();
+    // console.log(settingsSnapshot);
     // console.log(settingsSnapshot?.docs?.map(doc => doc.data()));
+  }
+
+  async modifyHeight(value: number) {
+    const newValue = this.masterExpenditureProgressHeight + value;
+
+    if( newValue >= 0.5 && newValue <= 2.5) {
+      this.masterExpenditureProgressHeight = newValue;
+      this.masterExpenditureProgressHeightNoEffectAfterUpdate = true;
+      setDoc(doc(this._fsService.db, 'settings', 'display', 'masterExpenditureProgressBar', 'height'), {
+        value: newValue
+      }).then();
+    }
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DashComponent } from './dash/dash.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
