e3bd325ad1598e18f3cd319e33f19639bab37080
HariPrasath <r.harip3@gmail.com>
2023-04-07 21:40:06 +0100

    build: carousel timing for qa

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,12 +2,14 @@ export const environment = {
     production: false,
 
     titleSuffix: " | QA",
-    
+
     apiKey: "AIzaSyBpozPb0T_ROyr86JPvmODBFOf4e4MojBA",
     authDomain: "budgets-qa-ee0c6.firebaseapp.com",
     projectId: "budgets-qa-ee0c6",
     storageBucket: "budgets-qa-ee0c6.appspot.com",
     messagingSenderId: "755476770742",
     appId: "1:755476770742:web:c761d3ae3229e928219b17",
-    measurementId: "G-1J90EYSDFH"
+    measurementId: "G-1J90EYSDFH",
+
+    crsl_interval: 3000,
   };
