57ece3627edeacd3492e96dfc846ec0de210232f
HariPrasath <r.harip3@gmail.com>
2023-04-11 21:05:00 +0100

    feat(BUD-21): show or hide cards

--- src/app/dash/metrics-carousel/cards/_models/Card.ts
+++ src/app/dash/metrics-carousel/cards/_models/Card.ts
@@ -1,20 +1,25 @@
 export type TCardType = 'transactionMetaReference' | 'accountsBalance';
 
+export interface ICard {
+
+  id: string;
+  order: number;
+  type: TCardType;
+  hidden: boolean;
+}
+
 export class Card {
   private _order: number;
   private _type: TCardType;
   private _id: string
-
-  public get order() {
-    return this._order
-  }
-
-  public get type() {
-    return this._type;
-  }
+  private _hidden: boolean;
 
   public get id() {
     return this._id;
+  }
+
+  public get order() {
+    return this._order
   }
 
   public set order(ord: number) {
@@ -22,10 +27,23 @@ export class Card {
     this.order = ord;
   }
 
-  constructor(id: string, order: number, type: TCardType) {
+  public get type() {
+    return this._type;
+  }
+
+  public get hidden() {
+    return this._hidden
+  }
+
+  public set hidden(hidden: boolean) {
+    this._hidden = hidden;
+  }
+
+  constructor(id: string, order: number, type: TCardType, hidden: boolean) {
 
     this._id = id;
     this._order = order;
     this._type = type;
+    this._hidden = hidden;
   }
 }
